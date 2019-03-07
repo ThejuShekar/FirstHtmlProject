a0265a5350a6aa441071e2f65c9cb1b9f81eb926
ThejuShekar <thejus90@gmail.com>
2019-03-07 13:52:26 -0500

    My HTML and Java

---
+++ Training Javascript/Size.js
@@ -0,0 +1,3 @@
+function changeSize(){
+	document.getElementById('msg').style.fontSize="30px";
+	}
---
+++ Training Javascript/Temperature.js
@@ -0,0 +1,14 @@
+
+function add()
+{
+var num1=Number(document.getElementById(num1)value);
+var num2=Number(document.getElementById(num2)value);
+var res=addNumbers(num1,num2);
+document.getElementById('result').innerhtml=res;
+}function addNumbers(x,y){
+return x + Y;
+}
+function farenheittocelcious (){
+	
+	
+}
